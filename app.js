156e56250298e7c4fab830566cf6e7c644ab6163
github username <github email address>
2020-01-09 09:57:38 -0700

    removed bug that printed span contents to screen when empty space inside button container was clicked

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 const screen = document.querySelector("#screen")
 const buttons = document.querySelector('.buttons')
 
-buttons.addEventListener('click', (event) => {
+buttons.addEventListener('click', (event) => {  
+    console.log(event.target) 
+    let clickedButtonText = event.target.textContent
+    let screenContents = screen.textContent
     
-    const clickedButton = event.target.innerHTML
-    
-    function clearScreen(){
-        screen.innerHTML=""
-    } 
+    let firstCharacterOnScreen = screenContents.charAt(0)
+    let lastCharacterOnScreen = screenContents.charAt(screenContents.length-1)
+    function addToScreen(click){screen.append(`${click}`)}
 
-    if (clickedButton!=="C" && clickedButton!=="="){
-        asInteger = parseInt(clickedButton)
-        if (asInteger){
-            screen.append(asInteger)
+    if (!isNaN(clickedButtonText)){
+        addToScreen(clickedButtonText)
+    } else if (clickedButtonText==="x"){
+        addToScreen('*')
+    } else if (clickedButtonText==="÷"){
+        addToScreen('/')
+    } else if (clickedButtonText==="+"){
+        addToScreen('+')
+    } else if (clickedButtonText==="-"){
+        addToScreen('-')
+    } else if (clickedButtonText==="C"){
+        clearScreen()
+    } else if (clickedButtonText==="="){
+        if (operatorTester(firstCharacterOnScreen, lastCharacterOnScreen)){
+            clearScreen()
+            screen.textContent="Error"} else {
+            let solution = eval(screenContents)
+            clearScreen()
+            screen.append(solution)
+            }
         }
-        // if (Number.isInteger(asInteger)){
-        //     screen.append(asInteger)}
-        else if (clickedButton==="+"){
-            screen.append("+")}
-        else if (clickedButton==="-"){
-            screen.append("-")}
-        else if (clickedButton==="÷"){
-            screen.append("/")}
-        else if (clickedButton==="x"){
-            screen.append("*")}
-    } else if (clickedButton=="C"){
-        clearScreen()
-    } else if (clickedButton=="="){
-        const solution = eval(screen.innerHTML)
-        clearScreen()
-        screen.append(solution)
-    }
+    } 
+)
 
-})
+function operatorTester(first, second){
+    if (first==="+" || first==="-" || first==="*" || first==="/" || second==="+" || second==="-" || second==="*" || second==="/"){
+        return true
+    } else {return false}
+}
+
+function clearScreen(){
+    screen.textContent=""
+}
+
+
+// clickedButtonText!=="C" && clickedButtonText!=="=" && clickedButtonText!=="x" && clickedButtonText!=="÷")
